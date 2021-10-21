805d61211054f9fd9d092fdc9f5690384c943eab
Ravi Kumar <ravikmar601@gmail.com>
2021-10-22 02:01:52 +0530

    created a navigation bar

--- server.js
+++ server.js
@@ -4,10 +4,25 @@ const fs = require("fs");
 const PORT = process.env.PORT || 4200;
 
 const server = http.createServer((req, res)=>{
+  console.log(req.url);
   if(req.url === '/'){
     fs.readFile("./public/index.html", (err, content)=>{
       if(err) throw err;
       res.writeHead(200, {"content-type" : "text/html"});
+      res.end(content);
+    });
+  }
+  if(req.url === '/src/index.css'){
+    fs.readFile("./src/index.css", (err, content)=>{
+      if(err) throw err;
+      res.writeHead(200, {"content-type" : "text/css"});
+      res.end(content);
+    });
+  }
+  if(req.url === '/src/index.js'){
+    fs.readFile("./src/index.js", (err, content)=>{
+      if(err) throw err;
+      res.writeHead(200, {"content-type" : "text/javascript"});
       res.end(content);
     });
   }
